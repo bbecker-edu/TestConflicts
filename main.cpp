a17624f87a484417336b01900daab60a188b0d6d
Balin Becker <bbecker@nic.edu>
2025-03-26 13:42:32 -0700

    Created add function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int add (int a, int b);
+
 int main() {
-    cout << "Greeting..." << endl;
     cout << "Hello World!" << endl;
     return 0;
+}
+
+int add (int a, int b) {
+    return a + b;
 }
